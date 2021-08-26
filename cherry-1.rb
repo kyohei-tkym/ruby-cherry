1577bb35b42b50371afd0f9b70ad85997708db2a
高山恭平 <kyohe1208@gmail.com>
2021-08-26 18:41:15 +0900

    ~p38

--- cherry-1.rb
+++ cherry-1.rb
@@ -2,6 +2,36 @@ puts 1 + 2
 
 a = 'Hello,World!'
 puts a
-
 b = 'こんにちは'
 puts b
+
+country = 'italy'
+
+if country == 'japan'
+    puts 'こんにちは'
+elsif country == 'us'
+    puts 'Hello'
+elsif country == 'italy'
+    puts 'ciao'
+else
+    puts '???'
+end
+
+country = 'us'
+
+if country == 'japan' then puts 'こんにちは'
+elsif country == 'us' then puts 'Hello'
+elsif country == 'italy' then puts 'ciao'
+else puts '???'
+end
+
+def greeting(country)
+    if country == 'japan'
+        puts 'こんにちは'
+    else
+        puts 'hello'
+    end
+end
+
+greeting('japan')
+greeting('us')
