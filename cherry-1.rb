701b25831b63a06a877ff0a0784667cf615f8112
高山恭平 <kyohe1208@gmail.com>
2021-08-27 17:07:19 +0900

    2章

--- cherry-1.rb
+++ cherry-1.rb
@@ -35,3 +35,57 @@ end
 
 greeting('japan')
 greeting('us')
+
+status = 'error'
+if status != 'ok'
+    puts '何か異常があります'
+end
+
+status = 'ok'
+unless status == 'ok'
+    puts '何か異常があります'
+else
+    puts '正常です'
+end
+
+country = 'italy'
+case country
+when 'japan'
+    puts 'こんにちは'
+when 'us'
+    puts 'hello'
+when 'italy'
+    puts 'ciao'
+else
+    puts '???'
+end
+
+country = '日本'
+case country
+when 'japan', '日本'
+    puts 'こんにちは'
+when 'us', 'アメリカ'
+    puts 'hello'
+when 'italy', 'イタリア'
+    puts 'ciao'
+else
+    puts '???'
+end
+
+n = 11
+puts n > 10 ? '10より大きい' : '10以下'
+
+n = 9
+message = n > 10 ? '10より大きい' : '10以下'
+puts message
+
+def greeting(country = 'japan')
+    if country == 'japan'
+        puts 'こんにちは'
+    else
+        puts 'hello'
+    end
+end
+
+puts greeting
+puts greeting('us')
