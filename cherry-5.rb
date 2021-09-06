874f92bb55465e8240d03cfff8b7e498cde169ef
高山恭平 <kyohe1208@gmail.com>
2021-09-06 18:04:14 +0900

    5章完了

--- cherry-5.rb
+++ cherry-5.rb
@@ -8,4 +8,18 @@ puts currencies['india']
 currencies = {'japan' => 'yen', 'us' => 'dollar', 'india' => 'rupee'}
 currencies.each do |key, value|
     puts " #{key} : #{value} "
-ends
+end
+
+h = Hash.new('hello')
+puts a = h[:foo]
+puts b = h[:bar]
+puts a.equal?(b)
+a.upcase!
+puts a
+puts b
+
+puts hash = {'abc': 123}
+
+puts %s(ruby is fun)
+
+puts %i(apple, orange, melon)
