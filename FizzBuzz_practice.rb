ad85de766efae9593266a8eb6637f80b563ddd05
高山恭平 <kyohe1208@gmail.com>
2021-10-24 17:39:34 +0900

    fizzbuzz practice

--- FizzBuzz_practice.rb
+++ FizzBuzz_practice.rb
@@ -1,3 +1,4 @@
+# eachで処理
 (1..30).each do |i|
     if i%15==0
         puts "FizzBuzz!"
@@ -7,5 +8,34 @@
         puts "Buzz!"
     else
         puts i
+    end
+end
+
+# whileで処理
+i=1
+while i <=30 
+
+    if i%15==0
+        puts "FizzBuzz!"
+    elsif i%3==0
+        puts "Fizz!"
+    elsif i%5==0 
+        puts "Buzz!"
+    else
+        puts i
+    end
+
+    i +=1
+end
+
+(1..30).each do |n|
+    if n % 15 == 0
+        puts "FizzBuzz"
+    elsif n % 5 == 0
+        puts "Buzz"
+    elsif n % 3 == 0
+        puts "Fizz"
+    else
+        puts n
     end
 end
